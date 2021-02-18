b0efb225e78efd26a92d1048f8478558d5a05945
Asia Zioła <ziola.jm@gmail.com>
2021-02-18 21:12:04 +0100

    [#3] initial config

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { appRouter } from "./routes/index";
+import mongoose from "mongoose";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || "development";
+let connection_uri =
+  process.env.MONGODB_DEV_URI || "mongodb://localhost/learning-tool-database";
+
+if (env === "production") {
+  connection_uri =
+    process.env.MONGODB_PROD_URI ||
+    "mongodb://localhost/learning-tool-database";
+}
+
+mongoose.connect(
+  connection_uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("connected to db");
+  }
+);
+
+app.use(appRouter);
+
+app.listen(port, function () {
+  console.log("App listening on port: " + port);
+});
