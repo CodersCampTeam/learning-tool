2c762db37869fbba3e001d647bbc09ea1d140095
mfrydrychowicz <mamgofry@gmail.com>
2021-02-19 22:17:32 +0100

    mongoose mongodb

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import { production_setup } from "./production";
+import mongoose from "mongoose"
 
 const app = express();
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || "development";
+let connection_uri = process.env.MONGODB_DEV_URI || 'mongodb://localhost/learning-tool-database';
 
-if (env !== "production") {
+if (env === "production") {
     production_setup(app);
+    connection_uri = process.env.MONGODB_PROD_URI
 }
+
+mongoose.connect(connection_uri);
 
 const mockResponse = {
     foo: "bar",
