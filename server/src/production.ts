1536d2a4df33e936e110e0e4827d2c9bfc45f93e
Asia <ziola.jm@gmail.com>
2021-02-20 22:28:14 +0100

    [#3] initial config (#4)
    * [#3] initial config

--- server/src/production.ts
+++ server/src/production.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import helmet from "helmet";
-import compression from "compression";
+import express from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
 
 export const productionSetup = (app: express.Express): void => {
     app.use(helmet());
