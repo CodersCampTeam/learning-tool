a51f36f05406b46587f658f3dab136cdc85466a8
pkowalczykk <pat.kowalczyk@wp.pl>
2021-03-20 19:06:05 +0100

    add @material-UI

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styles.css";
+import { Button } from '@material-ui/core'
+
+
+const App = () => {
+return (
+  <div className="App">
+    <Button>Button</Button>
+  </div>
+)
+}
+
+export default App;
