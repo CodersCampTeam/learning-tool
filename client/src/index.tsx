f6faf781f5d6574444d5518c6f8753a98d3c8ec9
pkowalczykk <pat.kowalczyk@wp.pl>
2021-03-20 23:27:43 +0100

    [#61] add stat

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import BasicTable from './statistics/statistics';
  
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BasicTable />
   </React.StrictMode>,
   document.getElementById('root')
 );
