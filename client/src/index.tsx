cea776c1b1aca10a881361e0a7e6acf662e4bec5
Asia <ziola.jm@gmail.com>
2021-03-23 10:38:46 +0100

    [#62] Material UI (#68)
    * add @material-UI
    
    * .tsx
    
    * add data-grid
    
    * [#62] emotion - initial setup
    
    * [#61] add stat
    
    * [#62] emotion - global, jsx pragma
    
    * [#62] themes
    
    * [#62] add palette
    
    * [#62] emotion again
    
    * [#62] add themes
    
    Co-authored-by: pkowalczykk <pat.kowalczyk@wp.pl>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,17 @@
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './views/App/App';
+import theme from './themes/theme';
+import { ThemeProvider } from '@emotion/react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<MuiThemeProvider theme={theme}>
+    		<ThemeProvider theme={theme}>
+				<App />
+      		</ThemeProvider>
+    	</MuiThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
