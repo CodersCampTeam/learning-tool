39ee987d05576cfb1de335366a6b4d2009a8c259
mfrydrychowicz <mamgofry@gmail.com>
2021-03-25 16:55:08 +0100

    [#70] no jednak sie da

--- client/src/components/NavBars/TopNavBar.tsx
+++ client/src/components/NavBars/TopNavBar.tsx
@@ -1,32 +1,26 @@
 import { FC, ReactElement } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { Search } from '../Search';
 import { StyledAppName } from '../../styles/StyledAppName';
+import styled from '@emotion/styled';
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1
-        },
-        title: {
-            paddingRight: '2em',
-            flexGrow: 1,
-            display: 'block'
-        }
-    })
-);
+export const StyledDiv = styled.div`
+    display: block;
+    flex-grow: 1;
+    padding-right: 2em;
+`;
 
 export const TopNavBar: FC = (): ReactElement => {
-    const classes = useStyles();
     return (
         <AppBar position="fixed">
             <Toolbar>
-                <StyledAppName className={classes.title}>
-                    <span>Fisz</span>
-                    <span>QI</span>
-                </StyledAppName>
+                <StyledDiv>
+                    <StyledAppName>
+                        <span>Fisz</span>
+                        <span>QI</span>
+                    </StyledAppName>
+                </StyledDiv>
                 <Search />
             </Toolbar>
         </AppBar>
