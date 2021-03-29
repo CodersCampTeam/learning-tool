024be78a2168790ab77b463124e9e33ffb7cf3fe
Patrycja <46416454+kowalczp@users.noreply.github.com>
2021-03-29 22:29:41 +0200

    [#73] collection-view (#84)
    Add flashcardCollectionView

---
+++ client/src/views/HomeView/FlashcardCollectionView.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactElement } from 'react'
+import CollectionView from '../../components/FlashcardCollectionComponents/flashcardCollections';
+
+export const HomeView: FC = (): ReactElement => {
+
+  return (
+    <CollectionView />
+  )
+}
