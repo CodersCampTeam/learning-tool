92425bf4632f647ee401ec7ed36f57529eebe308
mfrydrychowicz <mamgofry@gmail.com>
2021-03-30 16:58:27 +0200

    [#92] Fixed flashcardCollections formatting errors

--- client/src/components/FlashcardCollectionComponents/flashcardCollections.tsx
+++ client/src/components/FlashcardCollectionComponents/flashcardCollections.tsx
@@ -7,48 +7,48 @@ import { StyledGrid, CollectionHeader, Settings, AssessmentStyle, CreateCollecti
 import { grey } from '@material-ui/core/colors';
 
 const CollectionView = () => {
-	const url = '/api/flashcard-collection';
+    const url = '/api/flashcard-collection';
 
-	const [ data, setData ] = useState([]);
+    const [data, setData] = useState([]);
 
-	useEffect(() => {
-		axios.get(url).then((json) => setData(json.data));
-	}, []);
+    useEffect(() => {
+        axios.get(url).then((json) => setData(json.data));
+    }, []);
 
-	return (
-		<Container maxWidth="xs" justify-content="center">
-			<CreateCollection>
-				<div>Stwórz nową kolekcję</div>
-				<IconButton>
-					<AddCircle style={{ fontSize: 30, color: grey[800] }} />
-				</IconButton>
-			</CreateCollection>
-			{data.map((collection) => (
-				<StyledGrid>
-					<CollectionHeader>{collection['_id']}</CollectionHeader>
-					<div>
-						<IconButton>
-							<FeaturedPlayListOutlined />
-						</IconButton>
-						Fiszki: {collection['flashcards']}
-					</div>
-					<Settings>
-						<IconButton>
-							<BuildOutlined fontSize="large" style={{ color: grey[700], fontSize: 42 }} />
-						</IconButton>
-						<AssessmentStyle>
-							<IconButton>
-								<Assessment fontSize="large" style={{ color: grey[700], fontSize: 42 }} />
-							</IconButton>
-						</AssessmentStyle>
-						<IconButton>
-							<ArrowForward style={{ fontSize: 42, color: grey[700] }} />
-						</IconButton>
-					</Settings>
-				</StyledGrid>
-			))}
-		</Container>
-	);
+    return (
+        <Container maxWidth="xs" justify-content="center">
+            <CreateCollection>
+                <div>Stwórz nową kolekcję</div>
+                <IconButton>
+                    <AddCircle style={{ fontSize: 30, color: grey[800] }} />
+                </IconButton>
+            </CreateCollection>
+            {data.map((collection) => (
+                <StyledGrid>
+                    <CollectionHeader>{collection['_id']}</CollectionHeader>
+                    <div>
+                        <IconButton>
+                            <FeaturedPlayListOutlined />
+                        </IconButton>
+                        Fiszki: {collection['flashcards']}
+                    </div>
+                    <Settings>
+                        <IconButton>
+                            <BuildOutlined fontSize="large" style={{ color: grey[700], fontSize: 42 }} />
+                        </IconButton>
+                        <AssessmentStyle>
+                            <IconButton>
+                                <Assessment fontSize="large" style={{ color: grey[700], fontSize: 42 }} />
+                            </IconButton>
+                        </AssessmentStyle>
+                        <IconButton>
+                            <ArrowForward style={{ fontSize: 42, color: grey[700] }} />
+                        </IconButton>
+                    </Settings>
+                </StyledGrid>
+            ))}
+        </Container>
+    );
 };
 
 export default CollectionView;
