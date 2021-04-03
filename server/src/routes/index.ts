95946d1b7168d4d0b46fe90bbcf44587540aa92a
Asia Zioła <ziola.jm@gmail.com>
2021-04-03 17:59:16 +0200

    [#87] authorization

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -14,6 +14,7 @@ import passport from 'passport';
 import { runNotificationService } from '../services/NotificationService';
 import search from './search';
 import session from '../routes/Session';
+import me from './me';
 import * as path from 'path';
 
 const isAuthenticated = passport.authenticate('jwt', { session: false });
@@ -43,6 +44,8 @@ router.use('/api/flashcard-collection', isAuthenticated, flashcardCollection);
 router.use('/api/session', isAuthenticated, session);
 
 router.use('/api/search', isAuthenticated, search);
+
+router.use('/api/me', isAuthenticated, me);
 
 // for any other requests, send `index.html` as a response
 router.use(serveStatic(publicPath));
