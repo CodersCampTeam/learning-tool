51cc12a66b363989b1564bd926cb21c4aec4cece
pkowalczykk <pat.kowalczyk@wp.pl>
2021-04-07 00:03:14 +0200

    add collection statistics

--- server/src/routes/statistics.ts
+++ server/src/routes/statistics.ts
@@ -146,4 +146,98 @@ router.get('/collection', async (req: Request, res: Response, next) => {
     }
 });
 
+router.get('/collection', async (req: Request, res: Response, next) => {
+    const user = new Mongo.ObjectID(req['user']._id);
+    try {
+        FlashcardCollection.aggregate(
+            [
+                {
+                    $lookup: {
+                        from: 'flashcards',
+                        localField: '_id',
+                        foreignField: 'collectionId',
+                        as: 'flashcard'
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'answerhistories',
+                        let: { order_item: '$owner', order_qty: '$_id' },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $and: [
+                                            { $eq: ['$flashcardCollection', '$$order_qty'] },
+                                            { $eq: ['$user', '$$order_item'] }
+                                        ]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'stockdata'
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'answers',
+                        localField: 'stockdata.answers',
+                        foreignField: '_id',
+                        as: 'answers'
+                    }
+                },
+                {
+                    $addFields: {
+                        countAnswers: {
+                            $reduce: {
+                                input: '$stockdata.answers',
+                                initialValue: [],
+                                in: { $setUnion: ['$$value', '$$this'] }
+                            }
+                        },
+                        answers: { $arrayElemAt: ['$answers', 0] }
+                    }
+                },
+                { $unwind: { path: '$flashcards', preserveNullAndEmptyArrays: true } },
+                { $unwind: { path: '$countAnswers', preserveNullAndEmptyArrays: true } },
+                { $unwind: { path: '$stockdata', preserveNullAndEmptyArrays: true } },
+                { $unwind: { path: '$answers', preserveNullAndEmptyArrays: true } },
+                {
+                    $group: {
+                        _id: '$_id',
+                        CollectionName: { $first: '$name' },
+                        user: { $first: '$owner' },
+                        myflashcards: { $addToSet: '$flashcards' },
+                        answers: {
+                            $addToSet: {
+                                id: '$countAnswers',
+                                date: '$stockdata.sessionDate',
+                                isCorrect: '$answers.isCorrect'
+                            }
+                        }
+                    }
+                },
+                {
+                    $match: {
+                        user: user
+                    }
+                },
+                {
+                    $project: {
+                        CollectionName: 1,
+                        user: 1,
+                        myflashcards: { $sum: { $size: '$myflashcards' } },
+                        answers: 1
+                    }
+                }
+            ],
+            (err, results) => {
+                res.send(JSON.stringify(results, null, 2));
+            }
+        );
+    } catch (error) {
+        next(error);
+    }
+});
+
 export default router;
