27861894f90f2c32b2b6b00b1fce0df727e2e998
pkowalczykk <pat.kowalczyk@wp.pl>
2021-04-10 12:08:43 +0200

    corr get statistics

--- server/src/routes/statistics.ts
+++ server/src/routes/statistics.ts
@@ -93,75 +93,6 @@ router.get('/collection', async (req: Request, res: Response, next) => {
                 {
                     $lookup: {
                         from: 'answerhistories',
-                        localField: 'owner',
-                        foreignField: 'user',
-                        as: 'answers'
-                    }
-                },
-                {
-                    $addFields: {
-                        countAnswers: {
-                            $reduce: {
-                                input: '$answers.answers',
-                                initialValue: [],
-                                in: { $setUnion: ['$$value', '$$this'] }
-                            }
-                        }
-                    }
-                },
-                { $unwind: { path: '$countAnswers', preserveNullAndEmptyArrays: true } },
-                { $unwind: { path: '$flashcards', preserveNullAndEmptyArrays: true } },
-                { $unwind: { path: '$answers', preserveNullAndEmptyArrays: true } },
-                {
-                    $group: {
-                        _id: '$owner',
-                        mycollections: { $addToSet: '$name' },
-                        user: { $first: '$owner' },
-                        myflashcards: { $addToSet: '$flashcards' },
-                        session: { $addToSet: '$answers._id' },
-                        answerSession: { $addToSet: '$countAnswers' }
-                    }
-                },
-                {
-                    $match: {
-                        user: user
-                    }
-                },
-                {
-                    $project: {
-                        mycollections: { $sum: { $size: '$mycollections' } },
-                        user: 1,
-                        myflashcards: { $sum: { $size: '$myflashcards' } },
-                        answers: { $sum: { $size: '$answerSession' } },
-                        session: { $sum: { $size: '$session' } }
-                    }
-                }
-            ],
-            (err, results) => {
-                res.send(JSON.stringify(results, null, 2));
-            }
-        );
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get('/collection', async (req: Request, res: Response, next) => {
-    const user = new Mongo.ObjectID(req['user']._id);
-    try {
-        FlashcardCollection.aggregate(
-            [
-                {
-                    $lookup: {
-                        from: 'flashcards',
-                        localField: '_id',
-                        foreignField: 'collectionId',
-                        as: 'flashcard'
-                    }
-                },
-                {
-                    $lookup: {
-                        from: 'answerhistories',
                         let: { order_item: '$owner', order_qty: '$_id' },
                         pipeline: [
                             {
@@ -227,7 +158,112 @@ router.get('/collection', async (req: Request, res: Response, next) => {
                         CollectionName: 1,
                         user: 1,
                         myflashcards: { $sum: { $size: '$myflashcards' } },
-                        answers: 1
+                        answers: 1,
+                        finalMaxDate: {
+                            $slice: ['$max', 1]
+                        }
+                    }
+                }
+            ],
+            (err, results) => {
+                res.send(JSON.stringify(results, null, 2));
+            }
+        );
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/collections', async (req: Request, res: Response, next) => {
+    const user = new Mongo.ObjectID(req['user']._id);
+    try {
+        FlashcardCollection.aggregate(
+            [
+                { $sort: { max: -1 } },
+                {
+                    $lookup: {
+                        from: 'flashcards',
+                        localField: '_id',
+                        foreignField: 'collectionId',
+                        as: 'flashcard'
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'answerhistories',
+                        let: { order_item: '$owner', order_qty: '$_id' },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $and: [
+                                            { $eq: ['$flashcardCollection', '$$order_qty'] },
+                                            { $eq: ['$user', '$$order_item'] }
+                                        ]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'stockdata'
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'answers',
+                        localField: 'stockdata.answers',
+                        foreignField: '_id',
+                        as: 'answers'
+                    }
+                },
+                {
+                    $addFields: {
+                        countAnswers: {
+                            $reduce: {
+                                input: '$stockdata.answers',
+                                initialValue: [],
+                                in: { $setUnion: ['$$value', '$$this'] }
+                            }
+                        },
+                        answers: { $arrayElemAt: ['$answers', 0] }
+                    }
+                },
+                { $unwind: { path: '$flashcards', preserveNullAndEmptyArrays: true } },
+                { $unwind: { path: '$countAnswers', preserveNullAndEmptyArrays: true } },
+                { $unwind: { path: '$stockdata', preserveNullAndEmptyArrays: true } },
+                { $unwind: { path: '$answers', preserveNullAndEmptyArrays: true } },
+                {
+                    $group: {
+                        _id: {
+                            users: '$_id'
+                            // source: '$stockdata.sessionDate'
+                        },
+
+                        CollectionName: { $first: '$name' },
+                        user: { $first: '$owner' },
+                        myflashcards: { $addToSet: '$flashcards' },
+                        max: {
+                            $addToSet: {
+                                id: '$countAnswers',
+                                date: '$stockdata.sessionDate',
+                                isCorrect: '$answers.isCorrect'
+                            }
+                        }
+                    }
+                },
+                {
+                    $match: {
+                        user: user
+                    }
+                },
+                {
+                    $project: {
+                        CollectionName: 1,
+                        user: 1,
+                        myflashcards: { $sum: { $size: '$myflashcards' } },
+
+                        finalMaxDate: {
+                            $slice: ['$max.date', -1]
+                        }
                     }
                 }
             ],
